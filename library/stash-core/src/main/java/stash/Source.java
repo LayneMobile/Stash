1c907b69a8a2919e774d73bbad6a7adc70aba77d
Layne Penney <layne@laynemobile.com>
2016-04-25 05:18:25 -0500

    mapping out a way we could generate any functionality

--- library/stash-core/src/main/java/stash/Source.java
+++ library/stash-core/src/main/java/stash/Source.java
@@ -35,6 +35,113 @@ public interface Source<T, P extends Params> {
         }
     }
 
+    /*
+    Need to use annotations
+
+    @ProxyType(Source.class) or @ProxyType("Source")
+    class SourceProxy<T, P extends Params> {
+        @ProxyMethod("call")
+        Action2<P, Subscriber<? super T>> source;
+
+        @ProxyMethod("get")
+        Func0<T> value;
+
+        @ProxyTransform("call")
+        Action2<P, Subscriber<? super T>> source(final Action1<Subscriber<? super T>> source) {
+            return new Action2<P, Subscriber<? super T>>() {
+                @Override public void call(P p, Subscriber<? super T> subscriber) {
+                    source.call(subscriber);
+                }
+            };
+        }
+
+        @ProxyTransform("call")
+        Action2<P, Subscriber<? super T>> source(final Func1<P, T> source) {
+            return new Action2<P, Subscriber<? super T>>() {
+                @Override public void call(P p, Subscriber<? super T> subscriber) {
+                    try {
+                        T t = source.call(p);
+                        if (!subscriber.isUnsubscribed()) {
+                            subscriber.onNext(t);
+                            subscriber.onCompleted();
+                        }
+                    } catch (Throwable e) {
+                        if (!subscriber.isUnsubscribed()) {
+                            subscriber.onError(e);
+                        }
+                    }
+                }
+            };
+        }
+    }
+
+    // Generated Interface
+    interface Source<T, P extends Params> {
+        void call(P p, Subscriber<? super T> subscriber);
+
+        T get();
+    }
+
+    // Generated Invocation Handler
+    final class SourceHandler<T, P extends Params> implements MethodHandler {
+        private final Action2<P, Subscriber<? super T>> source;
+
+        private SourceHandler(Action2<P, Subscriber<? super T>> source) {
+            this.source = source;
+        }
+
+        @Override
+        @SuppressWarnings("unchecked")
+        public boolean handle(Object proxy, Method method, Object[] args, MethodResult result) throws Throwable {
+            Class<?>[] paramTypes = method.getParameterTypes();
+            if (paramTypes.length == 2
+                    && paramTypes[0].isAssignableFrom(Params.class)
+                    && paramTypes[1].isAssignableFrom(Subscriber.class)) {
+                source.call((P) args[0], (Subscriber<? super T>) args[1]);
+                return true;
+            }
+            return false;
+        }
+    }
+
+    // Generated Builder
+    final class SourceModule<T, P extends Params> implements TypeModule<Source> {
+        final ProxySource<T, P> proxy = new ProxySource<T, P>();
+
+        Builder<T, P> source(Action2<P, Subscriber<? super T>> source) {
+            proxy.source = source;
+            return this;
+        }
+
+        Builder<T, P> source(Action1<Subscriber<? super T>> source) {
+            proxy.source = proxy.source(source);
+            return this;
+        }
+
+        Builder<T, P> source(final Func1<P, T> source) {
+            proxy.source = proxy.source(source);
+            return this;
+        }
+
+        Builder<T, P> value(Func0<T> value) {
+            proxy.value = value;
+            return this;
+        }
+
+       @Override TypeHandler<Source> build() {
+            if (proxy.source == null) {
+                throw new IllegalStateException("source must be set");
+            }
+            return new TypeHandler.Builder<Source>(Source.class)
+                    .handle("call", new SourceHandler<T, P>(proxy.source))
+                    .build();
+        }
+    }
+    presents ->
+
+    @GenerateBuilder(OtherProxySource.class)
+    public interface ExtendedSource<T, P extends Params> implements Source<T, P> {}
+    */
 
     interface ProxyInterface {
         String name();
